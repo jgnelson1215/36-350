7b68ef5dbfb0fa776b4d10ec52d89e5d3c598c00
Julián Nelson <JulianNelson@Julians-MacBook-Pro.local>
2018-04-18 21:33:01 -0400

    original

---
+++ lab_12_simulation.R
@@ -0,0 +1,13 @@
+generate_data = function(n, p){
+  covariates = matrix(nrow = n, ncol = p)
+  for(i in 1:n){
+    trial = rnorm(n = p)
+    covariates[i, ] = trial
+  }
+  
+  responses = rnorm(n = n)
+  
+  return(list(covariates = covariates,
+              responses = responses)
+         )
+}
