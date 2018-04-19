868d9c24ae058e0a2c495bad1851748b51ff4e5f
Julián Nelson <JulianNelson@Julians-MacBook-Pro.local>
2018-04-18 22:38:01 -0400

    added model select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -10,3 +10,11 @@ generate_data = function(n, p){
   )
 }
 
+model_select = function(covariates, responses, cutoff){
+  model.lm = lm(responses ~ covariates)
+  valid.coefs = which(coef(model.lm)[, "Pr(>|t|)"] <= cutoff)
+  if(length(valid.coefs) == 0){return (c())}
+  new_model.lm = lm(responses ~ covariates[, valid.coefs])
+  return(coef(new_model.lm)[, "Pr(>|t|)"])
+}
+
